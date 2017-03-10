0ee434d989aecb3904796bd092ae451b2043f3d3
ZexDC <zexboost@gmail.com>
2017-03-10 11:30:52 +0000

    Added WCF project

---
+++ ACW_08346_464886_Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACW_08346_464886_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ACW_08346_464886_Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ACW_08346_464886_ServiceLibrary;
+
+namespace ACW_08346_464886_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost myHost = new ServiceHost(typeof(Service1));
+            myHost.Open();
+            Console.WriteLine("Server running...");
+            Console.ReadLine();
+            myHost.Close();
+        }
+    }
+}
